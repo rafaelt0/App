c69b9d5d5ba0abf7becaea05cb2363f75a03b00c
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-05 18:49:29 -0300

    Update 🧪_Simulação.py

--- pages/🧪_Simulação.py
+++ pages/🧪_Simulação.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.sidebar.header("Opções Simulação 👨‍🔬")
 mu_selected = st.sidebar.slider("Média",-4.00,4.00,0.01)
 sigma_selected = st.sidebar.slider("Volatilidade",-4.00,4.00,0.001)
 n_simulations = st.sidebar.slider("Número de Simulações",10,1000,10)
 valor = st.sidebar.number_input("Capital Inicial", min_value=10)
-periodos = int(st.sidebar.number_input("Período", min_value=2))
+periodos = int(st.sidebar.number_input("Passos", min_value=2))
+years = int(st.sidebar.number_input("Anos", min_value=2))
 
 try:
     def simulate_1d_gbm(nsteps=periodos, t=1, mu=mu_selected, sigma=sigma_selected):
@@ -22,41 +24,48 @@ except:
 
 
 
-try:
-    st.header("Simulação 💻")
-    st.subheader("Método de Monte Carlo")
-    import random
-    import plotly.express as px
-    nsims = n_simulations
-    simulation_data = {}
-    values=[]
-    for i in range(nsims):
-        x, y = simulate_1d_gbm()
-        simulation_data['y{col}'.format(col=i)] = y
-        values.append(y)
-        simulation_data['x'] = x
+st.header("Simulação 🧪")
+mu = mu_selected
+n = periodos
+M = n_simulations
+S0 = valor
+sigma = sigma_selected
+T = years
 
-    ycols = [ 'y{col}'.format(col=i) for i in range(nsims) ]
-    fig, ax = plt.subplots()
-    ax.plot(x, ycols)
-    st.pyplot(ax)
-    terminal_values=[values[i][-1] for i in range(nsims)]
-    terminal_values = np.array(terminal_values)
-    terminal_values = np.reshape(terminal_values,(-1,1))
-    df = pd.DataFrame([terminal_values.mean(), terminal_values.std(), terminal_values.max(), 
-                       terminal_values.min()])
-    df = df.rename({0:"Média", 1:"Desvio Padrão", 2:"Máximo", 3:"Mínimo"}, axis=0)
-    df = df.rename({0:"Estatísticas"}, axis=1)
-    st.subheader("Resultados")
-    st.dataframe(df.T)
-    
+dt = T/n
+
+St = np.exp(
+    (mu - sigma**2/2)*dt
+    * sigma * np.random.normal(0, np.sqrt(dt), size=(M,n)).T
+)
+
+St = np.vstack([np.ones(M), St])
+
+St = S0 * St.cumprod(axis=0)
+
+time = np.linspace(0,T,n+1)
+
+tt = np.full(shape=(M,n+1), fill_value=time).T
+
+fig=px.line(St, title="Simulação por Movimento Browniano Geométrico")
+fig.update_layout(
+                  xaxis = dict(
+                    tickmode='array', #change 1
+                    tickvals = np.arange(0,n*years,2)))
+fig.update_yaxes(title="Portfolio/Ação")
+fig.update_xaxes(title="Período")
+st.plotly_chart(fig)
+mean=St[-1][:].mean()
+max=St[-1][:].max()
+min=St[-1][:].min()
+array = np.array(St[-1][:])
+summary=pd.DataFrame([mean,max,min, np.percentile(array,25), np.median(array), np.percentile(array,75), np.std(array)])
+summary = summary.rename({0:"Média", 1:"Máximo", 2:"Mínimo", 3:"Primeiro Quartil", 4:"Mediana (Segundo Quartil)", 5:"Terceiro Quartil", 6:"Desvio Padrão"}, axis=0)
+summary = summary.rename({0:"Resultados"}, axis=1)
+st.subheader("Resultados 🔬")
+st.table(summary.T)
 
 
-
-
-
-except:
-    pass
 
 
 
