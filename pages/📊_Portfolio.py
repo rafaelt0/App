6d23b4145279ffdf00c2623b2bbd0b584abc4b99
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-05 13:22:39 -0300

    Update 📊_Portfolio.py

--- pages/📊_Portfolio.py
+++ pages/📊_Portfolio.py
@@ -6,7 +6,10 @@ import fundamentus
 import sklearn
 import pandas as pd
 import seaborn as sns
-from quantstats.stats import max_drawdown
+from quantstats.stats import sharpe, sortino, max_drawdown, risk_of_ruin, skew, kurtosis, var, volatility
+from quantstats.utils import download_returns
+from quantstats.plots import rolling_sharpe
+from quantstats.reports import full
 import warnings
 import pypfopt
 from pypfopt.expected_returns import mean_historical_return
@@ -25,9 +28,10 @@ lista.append("None")
 period_selected = st.sidebar.selectbox('Período ⏰', ['diário','semanal','trimestral','semestral','mensal','anual'])
 period_dict = {'diário':'1d','semanal':'1w','mensal':'1mo','trimestral':'3mo','semestral':'6mo','anual':'1y'}
 data_inicio = st.sidebar.date_input("Data Inicial📅", datetime.date(2024,1,1),min_value=datetime.date(2000,1,1))
-interval_selected = st.sidebar.selectbox('Intervalo 📊', ['dia','3 meses','mês','semana','hora','minuto'])
+interval_selected = st.sidebar.selectbox('Intervalo 📊', ['mês','3 meses','dia','semana','hora','minuto'])
 interval_dict={'dia':'1d','3 meses':'3mo', 'mês':'1mo','hora':'1h','minuto':'1m','semana':'1wk'}
 valor_inicial = st.sidebar.number_input("Valor Investido 💵", min_value=10, max_value=1_000_000)
+taxa_selic = st.sidebar.number_input("Taxa Selic 🪙 (%)", min_value=0.92, max_value=15.0)
 
 stocks = fundamentus.list_papel_all()
 tickers = list(st.sidebar.multiselect('Monte seu Portfolio (Escolha mais de uma ação)',stocks))
@@ -58,50 +62,85 @@ try:
     df = df.set_axis(["Descrição"], axis=1)
 except:
      pass
-try:
-    mu = mean_historical_return(data)
-    S = CovarianceShrinkage(data).ledoit_wolf()
-    ef = EfficientFrontier(mu, S)
-    ef.add_objective(objective_functions.L2_reg, gamma=0.1)
-    w = ef.max_sharpe()
-    weights=pd.DataFrame(ef.clean_weights(), index=[0])
-    weights=weights.rename({0:"Pesos"}, axis=0)
-    weights=round(weights,4)
-    weights_graph=np.array(weights).ravel()
-    weights_string= (weights*100).astype("str")+"%"
-    st.subheader("Porcentagem ótima no Portfolio")
-    fig, ax = plt.subplots(figsize=(12,5))
-    ax.pie(weights_graph,labels=weights.columns.values,autopct='%1.1f%%')
-    fig = px.pie(weights_graph, values=weights_graph, names=weights.columns.values)
-    st.plotly_chart(fig)
-    st.dataframe(weights_string)
-    weights=(weights*1_000_000).astype("int").T
-    returns_calc=(returns*1000_000).astype("int")
-    returns_calc=np.dot(returns_calc,weights)
-    returns=returns.index
-    returns_calc=pd.DataFrame(returns_calc/1_000_000_000_000)
-    returns_calc_non_pct=returns_calc/100
-    returns_calc.rename({0:"Retornos do Portfolio"},axis=1, inplace=True)
-    returns_calc.index=returns
-    returns_calc=round(returns_calc,3)
-    returns_calc_string = returns_calc.astype("str")+"%"
-    st.dataframe(returns_calc_string)
-    cum_return=(1+returns_calc_non_pct).cumprod()-1
-    cum_returns=round(cum_return*100,3)
-    cum_returns_string=cum_returns.astype("str")+"%"
-    cum_returns_df=pd.DataFrame(cum_returns_string)
-    cum_returns_df.index=returns
-    cum_returns_df=cum_returns_df.rename({0:"Retornos Acumulados Portfolio"}, axis=1)
-    st.dataframe(cum_returns_df)
-    valor=valor_inicial
-    portfolio_value=(1+cum_return)*valor
-    portfolio_value_df=pd.DataFrame(portfolio_value)
-    portfolio_value_df.index=returns
-    portfolio_value_df.rename({0:'Valor do Portfolio'}, axis=1, inplace=True)
-    st.write(portfolio_value_df)
-    st.write(max_drawdown(returns_calc_non_pct))
-except:
-    print("Monte seu Portfolio")
+
+mu = mean_historical_return(data)
+S = CovarianceShrinkage(data).ledoit_wolf()
+ef = EfficientFrontier(mu, S)
+ef.add_objective(objective_functions.L2_reg, gamma=0.1)
+w = ef.max_sharpe()
+weights=pd.DataFrame(ef.clean_weights(), index=[0])
+weights=weights.rename({0:"Pesos"}, axis=0)
+weights=round(weights,4)
+weights_graph=np.array(weights).ravel()
+weights_string= (weights*100).astype("str")+"%"
+st.subheader("Porcentagem ótima no Portfolio")
+fig, ax = plt.subplots(figsize=(12,5))
+ax.pie(weights_graph,labels=weights.columns.values,autopct='%1.1f%%')
+fig = px.pie(weights_graph, values=weights_graph, names=weights.columns.values)
+st.plotly_chart(fig)
+st.dataframe(weights_string)
+weights=(weights*1_000_000).astype("int").T
+returns_calc=(returns*1000_000).astype("int")
+returns_calc=np.dot(returns_calc,weights)
+returns=returns.index
+returns_calc=pd.DataFrame(returns_calc/1_000_000_000_000)
+returns_calc_non_pct=returns_calc/100
+returns_calc.index=returns
+returns_calc=round(returns_calc,3)
+returns_calc_string = returns_calc.astype("str")+"%"
+fig = px.line(returns_calc_non_pct.values,title="Retornos do Portfolio")
+fig.update_yaxes(title="Retornos")
+fig.update_xaxes(title="Período")
+
+st.plotly_chart(fig)
+fig= px.histogram(returns_calc_non_pct, title="Distribuição dos Retornos")
+fig.update_yaxes(title="Densidade")
+fig.update_xaxes(title="Retorno")
+fig.update_layout(
+    xaxis = dict(
+        tickmode = 'linear',
+        tick0 = -.3,
+        dtick = 0.025
+    )
+)
+st.plotly_chart(fig)
+st.dataframe(returns_calc_string)
+cum_return=(1+returns_calc_non_pct).cumprod()-1
+cum_returns=round(cum_return*100,3)
+cum_returns_string=cum_returns.astype("str")+"%"
+cum_returns_df=pd.DataFrame(cum_returns_string)
+cum_returns_df.index=returns
+cum_returns_df=cum_returns_df.rename({0:"Retornos Acumulados Portfolio"}, axis=1)
+st.dataframe(cum_returns_df)
+valor=valor_inicial
+portfolio_value=(1+cum_return)*valor
+portfolio_value_df=pd.DataFrame(portfolio_value)
+portfolio_value_df.index=returns
+portfolio_value_df.rename({0:'Valor do Portfolio'}, axis=1, inplace=True)
+fig = px.line(portfolio_value_df,title="Valor do Portfolio")
+fig.update_yaxes(title="Valor")
+fig.update_xaxes(title="Período")
+st.plotly_chart(fig)
+st.write(portfolio_value_df)
+stats=pd.DataFrame([sharpe(returns_calc_non_pct, rf=float(taxa_selic)/100), 
+                    sortino(returns_calc_non_pct, rf=float(taxa_selic)/100), 
+                    max_drawdown(returns_calc_non_pct),
+                    kurtosis(returns_calc_non_pct),
+                    volatility(returns_calc_non_pct),
+                    skew(returns_calc_non_pct)])
+stats=stats.T
+stats=stats.rename({0:"Índice Sharpe", 1:"Índice Sortino", 2:"Max Drawdown",
+                     3:"Curtose", 4:"Volatilidade", 5:"Assimetria"}, axis=1)
+stats=stats.rename({0:"Estatísticas"}, axis=0)
+returns_calc_non_pct.index=returns
+st.write(returns_calc_non_pct.shape)
+st.dataframe(stats)
+df_03 = download_returns(tickers)
+st.write(df_03.index)
+returns_calc=np.dot(df_03,weights)
+df=pd.DataFrame(returns_calc)
+df.index=df_03.index
+
 
 
 
