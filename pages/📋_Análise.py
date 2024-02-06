72b788b6867dc0f2663f9733a0c2f593db5ed2cb
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-06 03:00:05 -0300

    Update 📋_Análise.py

--- pages/📋_Análise.py
+++ pages/📋_Análise.py
@@ -38,7 +38,7 @@ with col3:
     st.write("")
 
 stocks = fundamentus.list_papel_all()
-tickers = list(st.multiselect('Monte seu Portfolio (Escolha mais de uma ação)', stocks))
+tickers = list(st.multiselect('Escolha ações para analisar', stocks))
                
 try:
     df = fundamentus.get_papel(list(tickers)[0])
