0c4058c8e52c01cde4925c47810a6374348f8816
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-06 23:22:52 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -54,7 +54,7 @@ try:
     for i in range(len(tickers)):
             df = pd.concat([df,fundamentus.get_papel(list(tickers)[i])])
 
-    
+    st.write(df)
     df_basic = df[['Empresa', 'Setor', 'Subsetor']]    
     st.write(df_basic.drop_duplicates(keep='last'))
     df_price = df[['Cotacao', 'Min_52_sem', 'Max_52_sem', 'Vol_med_2m', 'Valor_de_mercado', 'Data_ult_cot']]
