18b6d0dfbf02abea3e444ff9e9492992e9aa1e11
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-07 00:10:31 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -53,7 +53,7 @@ try:
     i=1
     for i in range(len(tickers)):
             df = pd.concat([df,fundamentus.get_papel(list(tickers)[i])])
-    df['PL'] = df['Valor_de_mercado'].astype('float64')/df['Lucro_Liquido_12m'].astype('float64')
+    df['PL'] = df['PL']/100
     df_basic = df[['Empresa', 'Setor', 'Subsetor']]    
     st.write(df_basic.drop_duplicates(keep='last'))
     df_price = df[['Cotacao', 'Min_52_sem', 'Max_52_sem', 'Vol_med_2m', 'Valor_de_mercado', 'Data_ult_cot']]
