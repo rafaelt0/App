980798557944a4eef551191c63c7b39e80541df1
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-06 23:02:15 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -57,7 +57,7 @@ try:
     df_basic = df[['Empresa', 'Setor', 'Subsetor']]    
     st.write(df_basic.drop_duplicates(keep='last'))
     df_price = df[['Cotacao', 'Min_52_sem', 'Max_52_sem']]
-    st.write(df_price.drop_duplicates(keep='last'))
+    st.dataframe(df_price.drop_duplicates(keep='last'))
     tickers = [ticker+".SA" for ticker in tickers]
     ticker = yf.Tickers(tickers)
 except:
