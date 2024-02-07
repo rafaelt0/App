d8894fcd9aba90e90b877d6e8e59c76b375dc8c2
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-07 00:01:49 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -58,7 +58,7 @@ try:
     st.write(df_basic.drop_duplicates(keep='last'))
     df_price = df[['Cotacao', 'Min_52_sem', 'Max_52_sem', 'Vol_med_2m', 'Valor_de_mercado', 'Data_ult_cot']]
     st.dataframe(df_price.drop_duplicates(keep='last'))
-    df_indicadores = df[['Marg_Liquida','ROE', 'ROIC', 'Div_Yield', 'Cres_Rec_5a']]
+    df_indicadores = df[['Marg_Liquida','ROE', 'ROIC', 'Div_Yield', 'Cres_Rec_5a', 'PL']]
     st.dataframe(df_indicadores.drop_duplicates(keep='last'))
     tickers = [ticker+".SA" for ticker in tickers]
     ticker = yf.Tickers(tickers)
