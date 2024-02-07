cb9b4a0fa57d1b7f46c84f4c47dfcdc9fa4a341e
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-06 21:40:30 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -88,6 +88,7 @@ try:
     df = pd.DataFrame(factors, index=lista)
     df = df.set_axis(["Descrição"], axis=1)
     st.subheader("Descrição da Empresa")
+    df = df.T
     st.table(df[['PL']])
 except:
      pass
