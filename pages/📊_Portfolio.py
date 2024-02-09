a856e353b13b518e716c1f812beaed7a4eb9ecaa
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-02-09 15:43:09 -0300

    Update 📊_Portfolio.py

--- pages/📊_Portfolio.py
+++ pages/📊_Portfolio.py
@@ -154,7 +154,7 @@ try:
     returns_calc_non_pct.index=returns
     st.dataframe(stats)
 except:
-    print("Hello World")
+    st.write("Portfolio inadequado, selecione outras ou mais ações")
     
 
 
