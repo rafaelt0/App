aa64a77660979551cd6dded9725f43937f54fb13
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-07-10 20:41:46 -0300

    Atualizar o 🧪_Simulação.py

--- pages/🧪_Simulação.py
+++ pages/🧪_Simulação.py
@@ -27,7 +27,7 @@ with col3:
 
 data = pd.read_csv('acoes-listadas-b3.csv')
 stocks = list(data['Ticker'].values)
-tickers = list(st.select('Escolha uma ação para simular', stocks))
+tickers = list(st.multiselect('Escolha uma ação para simular', stocks))
 
 try:
     df = fundamentus.get_papel(list(tickers)[0])
