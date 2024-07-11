1e67119299c3652b0e2bccaebee8c03075fc8d67
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-07-10 23:15:26 -0300

    Update 📊_Portfolio.py

--- pages/📊_Portfolio.py
+++ pages/📊_Portfolio.py
@@ -156,7 +156,10 @@ try:
     returns_calc_non_pct.index=returns
     st.dataframe(stats)
     statistics = returns_calc.mean(), returns_calc.median(), returns_calc.std(), returns_calc.max(), returns_calc.min()
-    st.write(statistics)
+    statistics_df = pd.DataFrame(statistics)
+    st.subheader("Outras Estatísticas Fundamentais")
+    statistics_df=statistics_df.set_axis(['Média','Mediana','Volatilidade','Máximo','Mínimo'], axis=0)
+    st.write(statistics_df)
 except:
     pass
 
