4e8fe490d92cd96cc9aec5ef1fe861c6e235c3d6
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-07-10 21:06:59 -0300

    Update 🧪_Simulação.py

--- pages/🧪_Simulação.py
+++ pages/🧪_Simulação.py
@@ -29,14 +29,8 @@ with col3:
 data = pd.read_csv('acoes-listadas-b3.csv')
 stocks = list(data['Ticker'].values)
 ticker = list(st.multiselect('Escolha uma ação para simular', stocks))
-
-
-df = fundamentus.get_papel(ticker)
 ticker = ticker+'.SA'
 ticker = yf.Tickers(ticker)
-
-
-
 data = ticker.history(start=data_inicio, end=datetime.datetime.now(),rounding=True)
 data = data.Close
 returns= data.pct_change()
