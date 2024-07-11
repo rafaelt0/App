c48f15897c42ef89d5f158abba7cbb9b95ba24d4
rafaelt0 <97866482+rafaelt0@users.noreply.github.com>
2024-07-10 22:45:12 -0300

    Update 📊_Portfolio.py

--- pages/📊_Portfolio.py
+++ pages/📊_Portfolio.py
@@ -161,7 +161,7 @@ except:
 result=st.button('Generate report')
 if result:
     st.write(returns_calc_non_pct)
-    quantstats.reports.plots(returns_calc_non_pct, output='pdf')
+    quantstats.reports.metrics(returns_calc_non_pct)
 else:
     print('Error')
 
