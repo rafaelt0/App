4b7592243c1d5d5c88225eec6d4909496c92f9ac
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-01 22:51:04 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -75,7 +75,7 @@ interval_dict={'dia':'1d','3 meses':'3mo', 'mês':'1mo','hora':'1h','minuto':'1m
 
 try:
     data = ticker.history(start=data_inicio, end=datetime.datetime.now()
-                          ,interval=interval_dict[interval_selected],rounding=True)
+                          ,rounding=True)
     st.write(data)
     data = data['Close']
     st.subheader("Cotação")
