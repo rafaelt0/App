5a1b14df6162c6de21cbfc8a939c2383f92e8a14
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-02 00:15:24 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -75,12 +75,12 @@ interval_dict={'dia':'1d','3 meses':'3mo', 'mês':'1mo','hora':'1h','minuto':'1m
 
 try:
     data = yf.download(tickers, start=data_inicio, end=datetime.datetime.now()).Close  
-    st.write(data.shape)
     data = data.reset_index()
     st.subheader("Cotação")
     st.write(data)
     returns= data.pct_change()
     returns = returns.dropna()*100
+    st.write(returns)
     returns_percentage = np.round(returns,2)
     returns_string = returns_percentage.astype(str)+'%'
     st.subheader("Retornos")
