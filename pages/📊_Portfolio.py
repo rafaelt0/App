606e94e507438ff9bf92da4a0d3b0f3cd4b9b436
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-02 00:48:11 -0300

    Update 📊_Portfolio.py

--- pages/📊_Portfolio.py
+++ pages/📊_Portfolio.py
@@ -53,8 +53,7 @@ except:
     pass
 
 try:
-    data = ticker.history(start=data_inicio, end=datetime.datetime.now(),period=period_dict[period_selected]\
-                          ,interval=interval_dict[interval_selected],rounding=True)
+    data = ticker.history(start=data_inicio, end=datetime.datetime.now(),period=period_dict[period_selected],rounding=True)
     data = data.Close
     st.write(data)
     bench = yf.Ticker("^BVSP")
