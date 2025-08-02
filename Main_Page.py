871ad23e8e8e477ad58c5d1de5e5aa1bdf88b730
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-01 23:47:33 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -74,7 +74,7 @@ interval_dict={'dia':'1d','3 meses':'3mo', 'mês':'1mo','hora':'1h','minuto':'1m
 
 
 try:
-    st.write(tickers)
+    st.write(list(tickers))
     data = yf.download(tickers, start=data_inicio, end=datetime.datetime.now())
     data = data['Close']
     data = data.reset_index()
