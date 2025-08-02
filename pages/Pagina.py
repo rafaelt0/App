020ae3b965d43a0ee5a05240b22767aada906aff
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-02 01:31:06 -0300

    Update Pagina.py

--- pages/Pagina.py
+++ pages/Pagina.py
@@ -28,7 +28,7 @@ st.sidebar.header('Configurações ⚙️')
 period_selected = st.sidebar.selectbox('Período ⏰', ['diário','semanal','trimestral','semestral','mensal','anual'])
 period_dict = {'diário':'1d','semanal':'1wk','mensal':'1mo','trimestral':'3mo','semestral':'6mo','anual':'1y'}
 
-data_inicio = st.sidebar.date_input("Data Inicial📅", datetime.date(2014,1,1),min_value=datetime.date(2000,1,1))
+data_inicio = st.sidebar.date_input("Data Inicial📅", datetime.date(2025,1,1),min_value=datetime.date(2000,1,1))
 interval_selected = st.sidebar.selectbox('Intervalo 📊', ['dia','mês','3 meses','semana','hora','minuto'])
 interval_dict={'dia':'1d','3 meses':'3mo', 'mês':'1mo','hora':'1h','minuto':'1m','semana':'1wk'}
 
