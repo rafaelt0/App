53d48afa9cbf2e1678874c58a6f53c1a2ccb59c5
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-01 23:48:38 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -74,8 +74,8 @@ interval_dict={'dia':'1d','3 meses':'3mo', 'mês':'1mo','hora':'1h','minuto':'1m
 
 
 try:
-    st.write(list(tickers))
     data = yf.download(tickers, start=data_inicio, end=datetime.datetime.now())
+    st.write(data)
     data = data['Close']
     data = data.reset_index()
     st.subheader("Cotação")
