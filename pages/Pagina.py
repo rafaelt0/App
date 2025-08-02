e8464d8aa0cbfe6c20ddaf063cb18117738899d7
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-02 03:54:01 -0300

    Update Pagina.py

--- pages/Pagina.py
+++ pages/Pagina.py
@@ -161,6 +161,73 @@ with tempfile.NamedTemporaryFile(suffix=".html", delete=False) as tmpfile:
         file_name="relatorio_portfolio.html",
         mime="text/html"
     )
+st.sidebar.header("Opções Simulação 👨‍🔬")
+n_simulations = st.sidebar.slider("Número de Simulações",10,1000,100)
+valor = st.sidebar.number_input("Capital Inicial", min_value=100)
+periodos = int(st.sidebar.number_input("Meses", value=12, min_value=1))
+years = int(st.sidebar.number_input("Anos", min_value=1))         
+data_inicio = st.sidebar.date_input("Data Inicial📅", value=datetime.date(2019,1,1),min_value=datetime.date(2000,1,1))
+
+
+st.header("Simulação 🧪")
+
+col1, col2, col3 = st.columns([1,3,1])
+
+with col1:
+    st.write("")
+
+
+with col3:
+    st.write("")
+     
+mean = float(portfolio_returns.mean())
+mu_selected = (1+mean)**12-1
+
+sigma_selected = portfolio_returns.std()*np.sqrt(12)
+    
+
+
+
+
+mu = mu_selected
+n = periodos
+M = n_simulations
+S0 = valor
+sigma = sigma_selected
+T = years
+
+dt = T/n
+
+St = np.exp(
+    (mu - sigma**2/2)*dt
+    * sigma * np.random.normal(0, np.sqrt(dt), size=(M,n)).T
+)
+
+St = np.vstack([np.ones(M), St])
+
+St = S0 * St.cumprod(axis=0)
+
+time = np.linspace(0,T,n+1)
+
+tt = np.full(shape=(M,n+1), fill_value=time).T
+
+fig=px.line(St, title="Simulação por Movimento Browniano Geométrico")
+fig.update_layout(
+                  xaxis = dict(
+                    tickmode='array', #change 1
+                    tickvals = np.arange(0,n*years,2)))
+fig.update_yaxes(title="Portfolio/Ação")
+fig.update_xaxes(title="Período")
+st.plotly_chart(fig)
+mean=St[-1][:].mean()
+max=St[-1][:].max()
+min=St[-1][:].min()
+array = np.array(St[-1][:])
+summary=pd.DataFrame([mean,max,min, np.percentile(array,25), np.median(array), np.percentile(array,75), np.std(array)])
+summary = summary.rename({0:"Média", 1:"Máximo", 2:"Mínimo", 3:"Primeiro Quartil", 4:"Mediana (Segundo Quartil)", 5:"Terceiro Quartil", 6:"Desvio Padrão"}, axis=0)
+summary = summary.rename({0:"Resultados"}, axis=1)
+st.subheader("Resultados 🔬")
+st.table(summary.T)
 
 
 
