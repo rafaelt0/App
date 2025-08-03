edb6059f5d81ba0d06d1a95c19e61e2041c8d945
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-03 12:06:43 -0300

    Atualizar o Pagina.py

--- pages/Pagina.py
+++ pages/Pagina.py
@@ -148,7 +148,7 @@ with aba1:
             cdi = sgs_series([12], start=data_inicio)["12"] / 100
             cdi_index = (1 + cdi).cumprod()
             benchmark_index = cdi_index / cdi_index.iloc[0]
-         else:
+        else:
             benchmark_index = None  # Caso nenhum benchmark selecionado
 
          if benchmark_index is not None:
