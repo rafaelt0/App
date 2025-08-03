740f990c0a44bc91e961954ded1d79fceb7deac1
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-03 09:10:41 -0300

    Update Initi.py

--- pages/Initi.py
+++ pages/Initi.py
@@ -110,6 +110,7 @@ with aba1:
     portfolio_value = (1 + portfolio_returns_alinhado).cumprod() * valor_inicial
     bench_value = (1 + retorno_bench_alinhado).cumprod() * valor_inicial
     st.write(bench_value)
+    st.write(portfolio_returns)
     # Plotly
     fig = go.Figure()
     fig.add_trace(go.Scatter(
