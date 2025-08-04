7bd9fe1ebdca637fd20db88e2f6d2f31b37c81b1
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-04 14:05:13 -0300

    Update Pagina.py

--- pages/Pagina.py
+++ pages/Pagina.py
@@ -228,6 +228,7 @@ with aba1:
     st.subheader("Retornos Anuais do Portfólio")
 
     fig_yearly = qs.plots.yearly_returns(portfolio_returns,benchmark=bench, show=False)
+    ax = plt.gca()
     ax.legend(['Portfólio', 'IBOVESPA'])  
     ax.set_title('Retornos Anuais (Portfólio vs IBOVESPA)')
     st.pyplot(fig_yearly)
