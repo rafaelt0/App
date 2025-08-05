fe96d5fa103e78292f2991839dcc5b5fba2fc4c2
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-05 00:49:09 -0300

    Update teste.py

--- pages/teste.py
+++ pages/teste.py
@@ -46,14 +46,12 @@ if benchmark_opcao == "IBOVESPA":
   bench_value = bench_cum * valor_inicial
 elif benchmark_opcao == "SELIC":
   bench = sgs.get({'selic': 432}, start=data_inicio)
-  selic_diario = (1+bench)**(1/30)-1
-  retorno_bench = selic_diario.pct_change().dropna()
+  retorno_bench = (1+bench)**(1/30)-1
   bench_cum = (1+selic_diario).cumprod()
   bench_value = bench_cum*valor_inicial
 else:
   bench = sgs.get({'CDI': 12}, start=data_inicio)
-  bench = bench/100
-  retorno_bench = bench.pct_change().dropna()
+  retorno_bench = bench/100
   bench_cum = (1+bench).cumprod()
   bench_value = bench_cum*valor_inicial
   
