ae8be4fb511e5733bff0ebae8a15dceb87761d3d
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-05 01:20:52 -0300

    Update teste.py

--- pages/teste.py
+++ pages/teste.py
@@ -342,29 +342,7 @@ st.pyplot(fig_3)
 
 
 
-# Botão para gerar PDF via quantstats
-import tempfile
-st.subheader("Baixar Relatório Completo (QuantStats)")
 
-# Converte para formato aceito pelo QuantStats
-portfolio_returns.index = pd.to_datetime(portfolio_returns.index)
-portfolio_returns = portfolio_returns.tz_localize(None)  # Remove timezone
-
-
-with tempfile.NamedTemporaryFile(suffix=".html", delete=False) as tmpfile:
-    qs.reports.html(
-        portfolio_returns,
-        benchmark= retorno_bench,
-        output=tmpfile.name,
-        title="Relatório Completo do Portfólio",
-        download_filename="relatorio_portfolio.html"
-    )
-    st.download_button(
-        label="Baixar Relatório HTML Completo (QuantStats)",
-        data=open(tmpfile.name, "rb").read(),
-        file_name="relatorio_portfolio.html",
-        mime="text/html"
-    )
 
 # Salva variáveis  para uso na aba Simulação
 st.session_state["modo"] = modo
@@ -406,6 +384,30 @@ fig_rc = px.bar(
     title="Contribuição de Risco por Ativo (%)"
 )
 st.plotly_chart(fig_rc, use_container_width=True)
+
+# Botão para gerar PDF via quantstats
+import tempfile
+st.subheader("Baixar Relatório Completo (QuantStats)")
+
+# Converte para formato aceito pelo QuantStats
+portfolio_returns.index = pd.to_datetime(portfolio_returns.index)
+portfolio_returns = portfolio_returns.tz_localize(None)  # Remove timezone
+
+
+with tempfile.NamedTemporaryFile(suffix=".html", delete=False) as tmpfile:
+    qs.reports.html(
+        portfolio_returns,
+        benchmark= retorno_bench,
+        output=tmpfile.name,
+        title="Relatório Completo do Portfólio",
+        download_filename="relatorio_portfolio.html"
+    )
+    st.download_button(
+        label="Baixar Relatório HTML Completo (QuantStats)",
+        data=open(tmpfile.name, "rb").read(),
+        file_name="relatorio_portfolio.html",
+        mime="text/html"
+    )
 
 
 
