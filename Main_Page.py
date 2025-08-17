dca9178f7e44590dee8d8da6b0b269c26d777e5a
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-17 20:26:51 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -46,7 +46,7 @@ with open("style.css") as f:
 st.title("**B3 Explorer 📈**")  
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-st.image("logo.jpg", width=400)
+    st.image("logo.jpg", width=400)
 # Carrega lista de ações da B3 com setores para filtragem inicial
 data = pd.read_csv('acoes-listadas-b3.csv')
 
