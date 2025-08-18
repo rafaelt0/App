83d49c7a1341c8a0db0cc18ccad32a866c0697d5
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-18 00:00:34 -0300

    Atualizar o Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -42,7 +42,8 @@ st.sidebar.success("Selecione uma página")
 with open("style.css") as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.image("b3explorer.png", width=450)
+st.image("b3explorer.png", width=350)
+
 # Carrega lista de ações da B3 com setores para filtragem inicial
 data = pd.read_csv('acoes-listadas-b3.csv')
 st.subheader("Explore ações da B3 🧭")
