d04a914c650c21b7d59c03b222ae069ef9a40566
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-18 09:38:36 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -234,7 +234,7 @@ if tickers:
         
         st.subheader("Cotação Histórica")
         st.write(data_prices)
-        st.line_chart(data_prices, y='ITUB4.SA')
+        st.line_chart(data_prices)
         # Histograma de distribuição de retornos
         st.subheader("Histograma Combinado dos Retornos Diários (%)")
         fig_hist_all = px.histogram(
