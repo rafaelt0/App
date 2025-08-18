f6f1e121e850d43ee8fcf1c24bc8d7e59b3da207
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-17 22:01:25 -0300

    Update 🧪_Simulação.py

--- pages/🧪_Simulação.py
+++ pages/🧪_Simulação.py
@@ -24,6 +24,9 @@ import io
 
 st.header("Simulação Monte Carlo por Ativos (Multivariada) 👨‍🔬")
 
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image("portfolio.png", width=400)
 
 
 # Verifica se as variáveis necessárias já estão no session_state
