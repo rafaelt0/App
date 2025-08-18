bd045bd3836b8538c78e8948132f8281c7a4d0aa
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-18 09:21:39 -0300

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -226,9 +226,11 @@ if tickers:
 
         # Garantir DataFrame caso seja Series
         if isinstance(data_prices, pd.Series):
-            data_prices = data_prices.to_frame(name=tickers_yf if isinstance(tickers_yf, str) else tickers_yf[0])
+            data_prices = data_prices.to_frame()
 
-
+        # Ajusta caso o DataFrame venha com MultiIndex de colunas
+        if isinstance(data_prices.columns, pd.MultiIndex):
+            data_prices = data_prices.droplevel(0, axis=1)
 
         st.subheader("Cotação Histórica")
 
