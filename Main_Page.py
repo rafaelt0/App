d1c4fe50c1f97a4b2e0cd748c3a3f3c0ab557576
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-17 23:59:54 -0300

    Atualizar o Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -45,6 +45,8 @@ with open("style.css") as f:
 st.image("b3explorer.png", width=450)
 # Carrega lista de ações da B3 com setores para filtragem inicial
 data = pd.read_csv('acoes-listadas-b3.csv')
+st.subheader("Explore ações da B3 🧭")
+tickers = st.multiselect('Escolha ações para explorar! (2 ou mais ações). Selecione a página e as configurações na aba lateral ', tickers_filtrados)
 
 if 'Setor' not in data.columns:
     st.error("O arquivo CSV precisa conter a coluna 'Setor' para o filtro funcionar.")
@@ -55,8 +57,6 @@ stocks = list(data['Ticker'].values)
 setores = sorted(data['Setor'].dropna().unique())
 setores.insert(0, "Todos")
 
-st.subheader("Explore ações da B3 🧭")
-tickers = st.multiselect('Escolha ações para explorar! (2 ou mais ações). Selecione a página e as configurações na aba lateral ', tickers_filtrados)
 
 # Permite filtro por setor na barra lateral
 setores_selecionados = st.multiselect(
