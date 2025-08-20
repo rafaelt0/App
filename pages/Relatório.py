cb7f8c6baea997c038b414511bec40fd0215be98
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-20 10:08:56 -0300

    Update Relatório.py

--- pages/Relatório.py
+++ pages/Relatório.py
@@ -20,14 +20,14 @@ portfolio_returns = portfolio_returns.tz_localize(None)  # Remove timezone
 
 with tempfile.NamedTemporaryFile(suffix=".html", delete=False) as tmpfile:
       qs.reports.html(
-        portfolio_returns,
-        benchmark= retorno_bench,
-        output=tmpfile.name,
-        title="Relatório Completo do Portfólio",
-        download_filename="relatorio_portfolio.html")
-   
-     st.download_button(
-        label="Baixar Relatório HTML Completo (QuantStats)",
-        data=open(tmpfile.name, "rb").read(),
-        file_name="relatorio_portfolio.html",
-        mime="text/html")
+      portfolio_returns,
+      benchmark= retorno_bench,
+      output=tmpfile.name,
+      title="Relatório Completo do Portfólio",
+      download_filename="relatorio_portfolio.html")
+      
+      st.download_button(
+      label="Baixar Relatório HTML Completo (QuantStats)",
+      data=open(tmpfile.name, "rb").read(),
+      file_name="relatorio_portfolio.html",
+      mime="text/html")
