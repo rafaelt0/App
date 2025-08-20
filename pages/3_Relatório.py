2ebc514c41318483c3108f8f3804ffebd90538bf
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-20 10:15:03 -0300

    Update 3_Relatório.py

--- pages/3_Relatório.py
+++ pages/3_Relatório.py
@@ -13,6 +13,15 @@ peso_manual_df = st.session_state["peso_manual_df"]
 portfolio_returns = st.session_state["portfolio_returns"]
 retorno_bench = st.session_state["retorno_bench"]
 
+
+# Verifica se as variáveis necessárias já estão no session_state
+required_keys = ["modo", "returns", "pesos_manuais", "peso_manual_df"]
+for key in required_keys:
+    if key not in st.session_state:
+        st.markdown("<br><br>", unsafe_allow_html=True)
+        st.warning("⚠️ Configure primeiro seu portfólio na aba 1 para liberar a geração do relatório.")
+        st.stop()
+          
 # Converte para formato aceito pelo QuantStats
 portfolio_returns.index = pd.to_datetime(portfolio_returns.index, errors='coerce')
 portfolio_returns = portfolio_returns.tz_localize(None)  # Remove timezone
