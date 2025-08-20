c80a5ea3f90eef96f9a56a16272e7b1e8ae08a77
Rafael Eiki Teruya <97866482+rafaelt0@users.noreply.github.com>
2025-08-20 10:17:35 -0300

    Update 3_Relatório.py

--- pages/3_Relatório.py
+++ pages/3_Relatório.py
@@ -6,14 +6,6 @@ import quantstats as qs
 import tempfile
 st.subheader("Baixar Relatório Completo (QuantStats)")
 
-modo = st.session_state["modo"]
-returns = st.session_state["returns"]
-pesos_manuais = st.session_state["pesos_manuais"]
-peso_manual_df = st.session_state["peso_manual_df"]
-portfolio_returns = st.session_state["portfolio_returns"]
-retorno_bench = st.session_state["retorno_bench"]
-
-
 # Verifica se as variáveis necessárias já estão no session_state
 required_keys = ["modo", "returns", "pesos_manuais", "peso_manual_df"]
 for key in required_keys:
@@ -21,6 +13,14 @@ for key in required_keys:
         st.markdown("<br><br>", unsafe_allow_html=True)
         st.warning("⚠️ Configure primeiro seu portfólio na aba 1 para liberar a geração do relatório.")
         st.stop()
+
+modo = st.session_state["modo"]
+returns = st.session_state["returns"]
+pesos_manuais = st.session_state["pesos_manuais"]
+peso_manual_df = st.session_state["peso_manual_df"]
+portfolio_returns = st.session_state["portfolio_returns"]
+retorno_bench = st.session_state["retorno_bench"]
+
           
 # Converte para formato aceito pelo QuantStats
 portfolio_returns.index = pd.to_datetime(portfolio_returns.index, errors='coerce')
@@ -28,15 +28,16 @@ portfolio_returns = portfolio_returns.tz_localize(None)  # Remove timezone
 
 
 with tempfile.NamedTemporaryFile(suffix=".html", delete=False) as tmpfile:
+      st.download_button(
+      label="Baixar Relatório HTML Completo (QuantStats)",
+      data=open(tmpfile.name, "rb").read(),
+      file_name="relatorio_portfolio.html",
+      mime="text/html")
+    
       qs.reports.html(
       portfolio_returns,
       benchmark= retorno_bench,
       output=tmpfile.name,
       title="Relatório Completo do Portfólio",
       download_filename="relatorio_portfolio.html")
-      
-      st.download_button(
-      label="Baixar Relatório HTML Completo (QuantStats)",
-      data=open(tmpfile.name, "rb").read(),
-      file_name="relatorio_portfolio.html",
-      mime="text/html")
+    
